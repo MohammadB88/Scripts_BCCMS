bcb5b489f18f2cc9e894379641a2b2a827bc9148
Mohammad Bahmani <45482139+MohammadB88@users.noreply.github.com>
2019-08-27 17:23:50 +0200

    Reading and manipulating H and DM suing sisl

--- pc_implementation.py
+++ pc_implementation.py
@@ -10,6 +10,7 @@ Created on Mon Aug 26 11:54:29 2019
 
 import sisl
 import math
+import numpy as np
 from scipy import constants
 
 # Assign the constant variables
@@ -25,10 +26,10 @@ distance_phsource = 5*(10**(-9)) # distance from light source to the monolayer s
 # reading the coordinates of the atoms at the device's boundary
 geom_fh = sisl.io.siesta.xvSileSiesta('/mnt/local/mb1988_data/mos2/devices/1t-2h-interface/armchair/arm2/device_width6/devgeom_constraint_SZP/scat_pristine/MoS2.XV')
 geom = geom_fh.read_geometry(species_Z=True)
-x_beg = geom[183][0]
-z_beg = geom[180][2]
-x_end = geom[182][0]
-z_end = geom[377][2]
+x_beg = geom[183,0]
+z_beg = geom[180,2]
+x_end = geom[182,0]
+z_end = geom[377,2]
 print('These are the device boundaries: \n x_beg:{}, x_end:{}, z_beg:{}, z_end:{}'.format(x_beg,z_beg,x_end,z_end)) # these coordinates are in Angstrom
 area_phproj = ((x_end-x_beg)*(z_end-z_beg))*(10**(-20)) # area on which the light is shined.
 V = distance_phsource * area_phproj # the volume in which light propagates from source to the monolayer surface under the projected light.
@@ -40,6 +41,9 @@ omega = E_gap / h_bar# the frequency of the light (for simplicity let's consider
 print(h_bar)
 #print(constants.value('Planck constant over 2 pi in eV s'))
 
+# read in and assign the device Hamiltonian using sisl (let's just try it out)
+H_0 = sisl.Hamiltonian.read('/mnt/local/mb1988_data/mos2/devices/1t-2h-interface/armchair/arm2/device_width6/devgeom_constraint_SZP/scat_pristine/MoS2.fdf')
+
 # it enforces the energy boundary of the perturbation
 #if energy_level = (-1)* h_bar * omega: which energy_level ????????????????
 #    delta_energy = 1
@@ -50,27 +54,62 @@ I_omega = (N * c)/(V * math.sqrt(mu_r * epsilon_r))
 
 print(I_omega)
 
-# it enforces to consider only Hamiltonian elements which are representing "occupied Orbital m and unoccupied Orbital l"
-#if m == occupied and l == unoccupied:
-#    AdaggerA = 1
+orbital_coordinate = np.zeros((9*198,2))
+new_i = 0
+for ai in range(0,198): # ai atom index
+   for oi in range(0,9): # oi orbital index
+       orbital_coordinate[new_i,0] = H_0.geometry.a2o(0, True)[oi]
+       orbital_coordinate[new_i,1] = geom[180+ai,2]
+       new_i = new_i + 1 # I need this so that these loops iterates and write all the atoms and their coordinate correctly.
+#
+#print(orbital_coordinate)
+#print(geom[180+197,2])
 
+atom_distance = np.zeros((198,9*198))
+for ai in range(0,198): 
+   for oi in range(0,9*198):
+       atom_distance[ai,oi] = geom[180+ai,2] - orbital_coordinate[oi,1] # z_m - z_l 
+
+# it enforces to consider only Hamiltonian elements which are representing "occupied Orbital m and unoccupied Orbital l"
 AdaggerA = 1 # to just see if the code works
 
-# read in and assign the device Hamiltonian using sisl (let's just try it out)
-H_0 = sisl.Hamiltonian.read('/mnt/local/mb1988_data/mos2/devices/1t-2h-interface/armchair/arm2/device_width6/devgeom_constraint_SZP/scat_pristine/MoS2.fdf')
 
-print([200 -- 300])
-print(H_0[3,4])
-print(H_0.geometry.a2o(0, True))
-print(H_0.geometry.a2o(197, True))
-print(9*197)
-print(H_0.geometry.a2o(2250, True))
+# read in and assign the device density matrix using sisl (let's just try it out)
+#DM = sisl.physics.DensityMatrix.read('/mnt/local/mb1988_data/mos2/devices/1t-2h-interface/armchair/arm2/device_width6/devgeom_constraint_SZP/scat_pristine/MoS2.fdf')
+fdf = sisl.get_sile('/mnt/local/mb1988_data/mos2/devices/1t-2h-interface/armchair/arm2/device_width6/devgeom_constraint_SZP/scat_pristine/MoS2.fdf')
+DM = fdf.read_density_matrix(order=['TSDE'])
+print(DM)
+print(DM[0,1])
+print(np.shape(DM))
+M = DM.copy() # M stores the mulliken charges
+M._csr._D[:,:] += DM._csr._D[:,-1].reshape(-1,1)
+print(M._csr._D[:,:])
+print(np.shape(M._csr._D[:,:]))
+print((DM._csr._D[:,-1].reshape(-1,1))[10,0])
+#M._csr._D[:,:] = np.multiply(M._csr._D[:,:], DM._csr._D[:,-1].reshape(-1,1))
+
+print(np.shape(M))
+for i in range(1):
+    for j in range(50):
+        if M[i,j].any() != 0:
+            print(M[i,j])
+print(M)
+
+#print(H_0[3,4])
+#print(H_0.geometry.a2o(0, True))
+#print(H_0.geometry.a2o(197, True))
+#print(9*197)
+#print(H_0.geometry.a2o(2250, True))
 
 # just to see if everything is working
 z_m = 10
 z_l = 5
 
+print(H_0)
+print(H_0[1,1,1])
+print(np.shape(H_0))
 # perturbation Hamiltonian
-H_perturbation = ((2 * pi_value * e_charge)/(h_bar)) * (z_m - z_l) * (((h_bar)/(2 * omega * epsilon * V))**(1/2)) * (N * delta_energy) * (H_0[3,4]) * (AdaggerA)
+H_perturbation = np.zeros(np.shape(H_0))
+H_perturbation = ((2 * pi_value * e_charge)/(h_bar)) * (z_m - z_l) * (((h_bar)/(2 * omega * epsilon * V))**(1/2)) * (N * delta_energy) * (H_0) * (AdaggerA)
 
-print(H_perturbation)
+#print(H_perturbation)
