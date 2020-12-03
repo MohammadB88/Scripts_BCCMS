8a979f0e5f012d3aac3e1e1b4c44c70a5804ae81
Mohammad Bahmani <45482139+MohammadB88@users.noreply.github.com>
2020-12-03 16:32:52 +0100

    Update plot_transmission_Mo_vacancies.py
    Zoom into specific parts of the plots and add corresponding inset plots

--- plot_transmission_Mo_vacancies.py
+++ plot_transmission_Mo_vacancies.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.ticker import ScalarFormatter
 
+# the inset represents the zoom-up of the small portion in the parent axes.
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
 #import glob # matches filenames and directories knowing some part of the name
 import numpy as np
 
@@ -15,11 +19,22 @@ lst_labels = ['Pristine', '$V_{Mo}$', '$V_{Mo3s}$', '$V_{Mo6s}$']
 # parameters for the plot
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10), squeeze=True)
 
+# set the title for left and right plots
+ax1.set_title("Bias 0.00 eV", fontsize=20, loc='center', pad=-25.0)
+ax2.set_title("Bias 1.40 eV", fontsize=20, loc='center', pad=-25.0)
+
+# create the box for the inset plots
+ax1_insetl = zoomed_inset_axes(ax1, 2, bbox_to_anchor=(0.33, 0.55), bbox_transform=ax1.transAxes, loc='lower left')
+ax1_insetr = zoomed_inset_axes(ax1, 2, bbox_to_anchor=(0.70, 0.55), bbox_transform=ax1.transAxes, loc='lower left')
+
+ax2_insetl = zoomed_inset_axes(ax1, 2, bbox_to_anchor=(1.30, 0.55), bbox_transform=ax1.transAxes, loc='lower left')
+ax2_insetr = zoomed_inset_axes(ax1, 2, bbox_to_anchor=(1.70, 0.55), bbox_transform=ax1.transAxes, loc='lower left')
+
 # Set labels, tick labels, parameters for all plots.
 for ax in (ax1,ax2):
     ax.set_xlim(-2.0,2.0)
     ax.set_xlabel('Energy (eV)', fontsize=20)
-    ax.set_ylim(0, 3.5)
+    ax.set_ylim(0, 3.3)
     ax.set_ylabel('Transmission', fontsize=20) #($10^{-10}$)
     ax.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
     ax.tick_params(axis='x', which='major', width=2.00, length=5.0, labelsize=20)
@@ -31,46 +46,112 @@ for ax in (ax1,ax2):
     #ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
     ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
     ax.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+    
+# **********************  AX1 **************************
+# ******************************************************
+# parameters for the inset box and plot at the LEFT side
+x1, x2, y1, y2 = -0.8, -0.3, -0.005, 0.5
+ax1_insetl.set_xlim(x1, x2)
+ax1_insetl.set_ylim(y1, y2)
+ax1_insetl.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+ax1_insetl.tick_params(axis='x', which='major', width=2.00, length=5.0, labelsize=20)
+ax1_insetl.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+ax1_insetl.tick_params(axis='x', which='minor', width=1.00, length=3.5, labelsize=14)
+ax1_insetl.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax1_insetl.yaxis.offsetText.set_fontsize(20)
+ax1_insetl.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+ax1_insetl.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+#ax1_insetl.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+#ax1_insetl.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
 
-ax1.set_title("Bias 0.00 eV", fontsize=20, loc='center', pad=-25.0)
-ax2.set_title("Bias 1.40 eV", fontsize=20, loc='center', pad=-25.0)
+# parameters for the inset box and plot at the RIGHT side
+x1, x2, y1, y2 = 1.0, 1.5, -0.005, 0.5
+ax1_insetr.set_xlim(x1, x2)
+ax1_insetr.set_ylim(y1, y2)
+ax1_insetr.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+ax1_insetr.tick_params(axis='x', which='major', width=2.00, length=5.0, labelsize=20)
+ax1_insetr.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+ax1_insetr.tick_params(axis='x', which='minor', width=1.00, length=3.5, labelsize=14)
+ax1_insetr.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax1_insetr.yaxis.offsetText.set_fontsize(20)
+ax1_insetr.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+ax1_insetr.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+#ax1_insetr.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+#ax1_insetr.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
 
+plt.xticks(visible=False)
+plt.yticks(visible=False)
 
+# **********************  AX2 **************************
+# ******************************************************
+# parameters for the inset box and plot at the LEFT side
+x1, x2, y1, y2 = -1.3, -0.6, -0.005, 0.5
+ax2_insetl.set_xlim(x1, x2)
+ax2_insetl.set_ylim(y1, y2)
+ax2_insetl.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+ax2_insetl.tick_params(axis='x', which='major', width=2.00, length=5.0, labelsize=20)
+ax2_insetl.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+ax2_insetl.tick_params(axis='x', which='minor', width=1.00, length=3.5, labelsize=14)
+ax2_insetl.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax2_insetl.yaxis.offsetText.set_fontsize(20)
+ax2_insetl.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+ax2_insetl.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+#ax2_insetl.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+#ax2_insetl.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
 
+# parameters for the inset box and plot at the RIGHT side
+x1, x2, y1, y2 = 1.4, 2.0, -0.005, 0.5
+ax2_insetr.set_xlim(x1, x2)
+ax2_insetr.set_ylim(y1, y2)
+ax2_insetr.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+ax2_insetr.tick_params(axis='x', which='major', width=2.00, length=5.0, labelsize=20)
+ax2_insetr.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+ax2_insetr.tick_params(axis='x', which='minor', width=1.00, length=3.5, labelsize=14)
+ax2_insetr.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax2_insetr.yaxis.offsetText.set_fontsize(20)
+ax2_insetr.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+ax2_insetr.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+#ax2_insetr.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+#ax2_insetr.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+plt.xticks(visible=False)
+plt.yticks(visible=False)
+
+# *************************************
 # read in and plot the pdos for TS_0.00
 for vacancy,label in zip(lst_vacancies,lst_labels):
     #print(vacancy, label)
     #print('PRINT',glob.glob('{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[0])))
     #f_l = np.loadtxt(glob.glob('{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[0]))[0], unpack=True)
-    print('PRINT', '{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[0]))
+    #print('PRINT', '{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[0]))
     f_l = np.loadtxt('{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[0]), unpack=True)
     ax1.plot(f_l[0], f_l[1], label='{}'.format(label))
-
-#ax2.set_xlim(-2.0,2.0)
-#ax2.set_xlabel('Energy (eV)', fontsize=20)
-#ax2.set_ylim(0, 100)
-#ax2.set_ylabel('Projected DOS', fontsize=20) #($10^{-10}$)
-#ax2.set_title("Bias 1.40 eV", fontsize=20, loc='center', pad=-25.0)
-
-#ax2.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
-#ax2.tick_params(axis='x', which='major', width=2.00, length=5.0, labelsize=20)
-#ax2.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
-#ax2.tick_params(axis='x', which='minor', width=1.00, length=3.5, labelsize=14)
-#ax2.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-#ax2.yaxis.offsetText.set_fontsize(20)
-#ax2.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
-##ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
-#ax2.yaxis.set_minor_locator(ticker.MultipleLocator(10))
-#ax2.tick_params(axis='y', which='major', width=2.00, length=5.0, labelsize=22)
+    
+    # plot the inset image
+    ax1_insetl.plot(f_l[0], f_l[1], label='{}'.format(label))
+    ax1_insetr.plot(f_l[0], f_l[1], label='{}'.format(label))
+    
+    # draw a bbox of the region of the inset axes in the parent axes and
+    # connecting lines between the bbox and the inset axes area
+    mark_inset(ax1, ax1_insetl, loc1=2, loc2=4, fc="none", ec="0.5")
+    mark_inset(ax1, ax1_insetr, loc1=2, loc2=4, fc="none", ec="0.5")
 
 # read in and plot the pdos for TS_1.40
 for vacancy,label in zip(lst_vacancies,lst_labels):
     #print(vacancy, label)
     #print('PRINT',glob.glob('{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[1])))
     #f_l = np.loadtxt(glob.glob('{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[1]))[0], unpack=True)
-    print('PRINT', '{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[1]))
+    #print('PRINT', '{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[1]))
     f_l = np.loadtxt('{}/{}MoS2_dev.TBT.AVTRANS_Left-Right'.format(vacancy,direc[1]), unpack=True)
     ax2.plot(f_l[0], f_l[1], label='{}'.format(label))
+    
+    # plot the inset image
+    ax2_insetl.plot(f_l[0], f_l[1], label='{}'.format(label))
+    ax2_insetr.plot(f_l[0], f_l[1], label='{}'.format(label))
+    
+    # draw a bbox of the region of the inset axes in the parent axes and
+    # connecting lines between the bbox and the inset axes area
+    mark_inset(ax2, ax2_insetl, loc1=2, loc2=4, fc="none", ec="0.5")
+    mark_inset(ax2, ax2_insetr, loc1=2, loc2=4, fc="none", ec="0.5")
 
 # Hide x labels and tick labels for top plots and y ticks for right plots.
 for ax in (ax1,ax2):
