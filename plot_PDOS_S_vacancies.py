284be6df2c480eec67207169ff7be5e42678e998
Mohammad Bahmani <45482139+MohammadB88@users.noreply.github.com>
2021-02-15 11:40:17 +0100

    Update plot_PDOS_S_vacancies.py

--- plot_PDOS_S_vacancies.py
+++ plot_PDOS_S_vacancies.py
@@ -9,7 +9,7 @@ direc = ['TS_0.00/', 'TS_1.40/']
 
 layer = [2]
 lst_vacancies = ['pristine', 'int_LR_v_s', 'int_LR_v_2stop', 'int_LR_v_2spar']
-lst_labels = ['Pristine', '$V_{S}$', '$V_{2S-top}$', '$V_{2S-par}$']
+lst_labels = ['Perfect Int', '$V_{S}$', '$V_{2S-top}$', '$V_{2S-par}$']
 fermi_shifts = [00.0000000000, 0.005007000000000428, 0.0030080000000003437, -0.005323999999999884]
 
 # parameters for the plot
@@ -19,13 +19,13 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 7), sharex=True, squeeze=True)
 fig.add_subplot(111, frameon=False)
 # hide tick and tick label of the big axis
 plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-plt.xlabel('Energy (eV)', fontsize=20, labelpad=12.0)
+plt.xlabel('$E$ - $E_{F}$ (eV)', fontsize=20, labelpad=12.0)
 #plt.ylabel('Projected Local DOS (Left Interface)', fontsize=20, labelpad=15.0)
-plt.ylabel('LDOS', fontsize=20, labelpad=15.0)
+plt.ylabel('LDOS (arb. units)', fontsize=20, labelpad=15.0)
 
 # set the title for left and right plots
-ax1.set_title("V = 0.00", fontsize=20, loc='center', pad=-26.0)
-ax2.set_title("V = 1.40", fontsize=20, loc='center', pad=-26.0)
+ax1.set_title("V = 0.00", fontsize=19, loc='center', pad=-26.0, fontweight="bold")
+ax2.set_title("V = 1.40", fontsize=19, loc='center', pad=-26.0, fontweight="bold")
 
 # Set labels, tick labels, parameters for all plots.
 for ax in (ax1,ax2):
@@ -43,16 +43,21 @@ for ax in (ax1,ax2):
     ax.yaxis.set_major_locator(ticker.MultipleLocator(10))
     #ax.yaxis.set_minor_locator(ticker.MultipleLocator(10))
     ax.tick_params(axis='y', which='minor', width=1.00, length=3.5, labelsize=14, direction='in', left=True, right=True)
-
+    
+    # make spines (Frames) thicker
+    ax.spines["top"].set_linewidth(2)
+    ax.spines["bottom"].set_linewidth(2)
+    ax.spines["left"].set_linewidth(2)
+    ax.spines["right"].set_linewidth(2)
 
 # read in and plot the pdos for TS_0.00
 for vacancy, label, f_shift in zip(lst_vacancies, lst_labels, fermi_shifts):
     #print(vacancy, label)
     #print('PRINT',glob.glob('{}/{}dos_Left_int_{}L_*.dat'.format(vacancy,direc[0],layer[0])))
     f_l = np.loadtxt(glob.glob('{}/{}dos_Left_int_{}L_*.dat'.format(vacancy,direc[0],layer[0]))[0], unpack=True)
-    print(f_shift)
-    #ax1.plot(f_l[0], f_l[1], label='{}'.format(label))
-    ax1.plot(f_l[0] + f_shift, f_l[1], label='{}'.format(label))
+    #print(f_shift)
+    ax1.plot(f_l[0], f_l[1], label='{}'.format(label))
+    #ax1.plot(f_l[0] + f_shift, f_l[1], label='{}'.format(label))
     
 
 # read in and plot the pdos for TS_1.40
@@ -60,8 +65,8 @@ for vacancy, label, f_shift in zip(lst_vacancies, lst_labels, fermi_shifts):
     #print(vacancy, label)
     #print('PRINT',glob.glob('{}/{}dos_Left_int_{}L_*.dat'.format(vacancy,direc[1],layer[0])))
     f_l = np.loadtxt(glob.glob('{}/{}dos_Left_int_{}L_*.dat'.format(vacancy,direc[1],layer[0]))[0], unpack=True)
-    #ax2.plot(f_l[0], f_l[1], label='{}'.format(label))
-    ax2.plot(f_l[0] + f_shift, f_l[1], label='{}'.format(label))
+    ax2.plot(f_l[0], f_l[1], label='{}'.format(label))
+    #ax2.plot(f_l[0] + f_shift, f_l[1], label='{}'.format(label))
 
 # Hide x labels and tick labels for top plots and y ticks for right plots.
 for ax in (ax1,ax2):
@@ -74,11 +79,11 @@ fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 1.01), ncol
 
 #fig.text(0.5, 0.92, 'Projected Local DOS onto the layers at the left interface', fontsize=20, horizontalalignment='center', verticalalignment='top')
 
-plt.tight_layout(rect=(-0.03,-0.04,1.015,0.925))
+plt.tight_layout(rect=(-0.04,-0.05,1.015,0.925))
 #plt.subplots_adjust(left=0.01, right=0.02, wspace=0.00, hspace=0.0, bottom=0.0,top=0.35)
 
 
-#plt.savefig('PLDOS_S_vacancies.png')
-plt.savefig('PLDOS_S_vacancies_shift.png')
+plt.savefig('PLDOS_S_vacancies.png')
+#plt.savefig('PLDOS_S_vacancies_shift.png')
 
-plt.show()
+#plt.show()
